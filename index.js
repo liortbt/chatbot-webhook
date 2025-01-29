75da19130e6553d7d20b4a481728352b62bc2977
lior.tbt <lior@tbt.com>
2025-01-29 15:07:44 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+const VERIFY_TOKEN = process.env.MYSECURETOKEN; // Replace with your chosen token
+
+app.get("/webhook", (req, res) => {
+
+        console.log(req);
+        res.status(200).send("challenge"); // Respond with the challenge token
+    
+});
+
+app.post("/", (req, res) => {
+    console.log(req);
+    res.status(200).send("challenge"); // Respond with the challenge token
+
+});
+
+app.listen(3000, () => console.log("Server running on port 3000"));
