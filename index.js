d56cfe3212fc1e7a86eabb25b59adb3397d63e8e
lior.tbt <lior@tbt.com>
2025-01-29 15:42:19 +0200

    Added challange

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 import express from "express";
 const app = express();
 
-const VERIFY_TOKEN = process.env.MYSECURETOKEN; // Replace with your chosen token
+const VERIFY_TOKEN = "mysecuretoken123"; // Use the same token as in Facebook Console
 
 app.get("/webhook", (req, res) => {
-        res.status(200).send("challenge"); // Respond with the challenge token
-    
+    const mode = req.query["hub.mode"];
+    const token = req.query["hub.verify_token"];
+    const challenge = req.query["hub.challenge"];
+
+    if (mode === "subscribe" && token === VERIFY_TOKEN) {
+        console.log("Webhook verified successfully!");
+        res.status(200).send(challenge); // Send back the challenge token
+    } else {
+        res.sendStatus(403); // Forbidden
+    }
 });
 
 app.post("/webhook", (req, res) => {
-    console.log(req);
-    res.status(200).send("challenge"); // Respond with the challenge token
-
+    console.log("Webhook POST request received");
+    res.status(200).send("Webhook POST request successful");
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(3000, () => console.log("Webhook running on port 3000"));
